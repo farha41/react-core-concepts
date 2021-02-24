a7d7d1c93be0a78ad1c0010d9c21994562557c28
farha41 <farhamahjaben8@gmail.com>
2021-02-24 12:27:19 +0600

    adding style on react file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name:"eva rahman",
+    job: "singer"
+  }
+  var style ={
+    color:'red',
+    backgroundColor:'grey'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1>my heading</h1>
+        <h3 style={style}>singer:{person.name+" "+person.job}</h3>
+        <p style={{color:'orange',backgroundColor:'cyan'}}>my first react app</p>
         <a
           className="App-link"
           href="https://reactjs.org"
